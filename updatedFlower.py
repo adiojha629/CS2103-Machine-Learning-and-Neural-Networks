84e9c969e3e4fed4bcd06779a6aa93c65e070a7c
Steven J. Foland <stevenfoland@tsogiants.org>
2019-06-15 15:37:40 -0500

    Update updatedFlower.py

--- updatedFlower.py
+++ updatedFlower.py
@@ -3,10 +3,10 @@ import numpy as np
 
 
 # goes length, width, color(red =1, blue -0)
-data = [{'data': [3,1.5], 'prediction': 1}, {'data':[2,1], 'prediction': 0},
-        {'data': [4,1.5], 'prediction': 1}, {'data': [3,1], 'prediction': 0},
-        {'data': [3.5,.5], 'prediction': 1}, {'data': [2,.5], 'prediction': 0},
-        {'data': [5.5,1], 'prediction': 1}, {'data': [1,1], 'prediction': 0}]
+data = [{'data': [3,1.5], 'target': 1}, {'data':[2,1], 'target': 0},
+        {'data': [4,1.5], 'target': 1}, {'data': [3,1], 'target': 0},
+        {'data': [3.5,.5], 'target': 1}, {'data': [2,.5], 'target': 0},
+        {'data': [5.5,1], 'target': 1}, {'data': [1,1], 'target': 0}]
 
 mystery_flower = {'data': [4.5,1]}
 
@@ -21,36 +21,48 @@ def sigmoid_p(x):#derivative of sigmoid
     return sigmoid(x)*(1-sigmoid(x))
 
 ### Testing results with Test Data
-def accuracy():
+def accuracy(w):
     correct = 0;
     trials = 100
     for i in range(trials):
         ri = np.random.randint(len(data)) #random index that falls inside data's index's range
         point = data[ri]
 
-        z = point[0]*w1 + point[1]*w2 +b
-        pred = sigmoid(z)
-        target = point[2]
+        pred = fwdProp(point, w)
+
+        target = point['target']
 
         if (pred > .5 and target == 1) or (pred < .5 and target == 0): 
             correct += 1
     #print("Correct "+ str(correct*100/trials))
     return correct
 
-def propogate(p, w):
+    
+def fwdProp(p, w):
     ### Forward propogation
     if(len(p['data']) == len(w)-1):
         pred = 0
         for i in range(len(p['data'])):
-            pred += p[i]*w[i]
+            pred += p['data'][i]*w[i]
         pred = sigmoid(pred + w[-1]) # Add in the bias and normalize.
-        out = {'prediction': pred}
+        return pred
     else:
         return False
-    
-    
+  
+def backProp(p,w):
     ### Backward propogation
-    if()
+    H = 1e-18 # Might have to change later.
+    if(p['target']):
+        d_costs = []
+        for i in range(len(w)):
+            w_temp1 = w
+            w_temp2 = w
+            w_temp1[i] += H
+            w_temp2[i] -= H
+            d_costs.append((fwdProp(p,w_temp1)-fwdProp(p,w_temp2))/(2*H))
+        return d_costs     
+    else:
+        return False
 
 
 ###training loop
@@ -60,29 +72,16 @@ weight = [np.random.randn(), np.random.randn(), np.random.randn()]
 #print(accuracy())
 #accuracyList.append(accuracy())
 
-for i in range(1000):
+for i in range(10000):
     ri = np.random.randint(len(data)) #random index that falls inside data's index's range
-    result = propogate(data[ri], weight)    ### Need cost to be a list with lenght 4 (?)
+    prediction = fwdProp(data[ri], weight)    ### Need cost to be a list with lenght 4 (?)
     
-
-    pred = sigmoid(z)
-    target = point[2]
-    cost = np.square(pred - target)
+    d_costs = backProp(data[ri], weight)
     
-    dCost_pred = 2*(pred-target)
-    dpred_z = sigmoid_p(z)
-    
-    dz_dw1 = point[0]
-    dz_dw2 = point[1]
-    dz_db = 1
-    
-    dCost_dw1 = dCost_pred*dpred_z*dz_dw1
-    dCost_dw2 = dCost_pred*dpred_z*dz_dw2
-    dCost_db = dCost_pred*dpred_z*dz_db
-    
-    w1 -= learning_rate*dCost_dw1
-    w2 -= learning_rate*dCost_dw2
-    b -= learning_rate*dCost_db
+    if(d_costs):
+        weight[0] -= LEARNING_RATE*d_costs[0]
+        weight[1] -= LEARNING_RATE*d_costs[1]
+        weight[2] -= LEARNING_RATE*d_costs[2]
         
     #if i % 100 == 0:
         #print("Training Session: "+ str(i))
@@ -98,15 +97,12 @@ for i in range(1000):
         #costsList.append(costSum/len(data))   
              
 print("End of Training")
-print(str(w1))
-print(str(w2))
-print(str(b))
-accuracy()
+print(str(weight[0]))
+print(str(weight[1]))
+print(str(weight[2]))
+accuracy(weight)
 
-point = mystery_flower
-    
-z = point[0]*w1 + point[1]*w2 +b
-pred = sigmoid(z)
+pred = fwdProp(mystery_flower, weight)
 
 print("Prediction for Mystery Flower: "+ str(pred))
 
